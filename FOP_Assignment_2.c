148f3afc8e727b1c152d53f8b3d1f5c297c02c7a
Akshat Agrawal <gopalagarwal8899@gmail.com>
2026-02-20 10:22:56 +0530

    Second Assignment

---
+++ FOP_Assignment_2.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float num1 , num2 , result;
+
+    // Display Menu
+    printf("====MENU=====\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Enter your choice");
+    scanf("%d",&choice);
+
+    // Input Numbers
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &num1, &num2);
+
+    // switch-case for Arithmetic Operators:
+    
+    switch(choice) {
+        case 1:
+            result = num1 + num2;
+            printf("Addition = %.2f\n", result);
+            break;
+
+        case 2:
+            result = num1 - num2;
+            printf("Subtraction = %.2f\n", result);
+            break;
+
+        case 3:
+            result = num1 * num2;
+            printf("Multiplication = %.2f\n", result);
+            break;
+
+        case 4:
+            if(num2 != 0) {
+                result = num1 / num2;
+                printf("Division = %.2f\n", result);
+            } else {
+                printf("Error! Division by zero is not allowed.\n");
+            }
+            break;
+
+        default:
+            printf("Invalid choice! Please select between 1 to 4.\n");
+    }
+
+    return 0;
+}
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
