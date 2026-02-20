6fc5542e79cc97c11e36aee937589faa2f338365
Akshat Agrawal <gopalagarwal8899@gmail.com>
2026-02-20 08:47:47 +0530

    Name Change of File

---
+++ FOP_Assignment_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int year;
+
+    printf("Please enter a year you want to check: ");   // Leap year program
+    scanf("%d" , &year);
+    
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        printf("%d is a Leap Year\n",year);
+    } else {
+        printf("%d is Not a Leap Year\n", year);
+    }
+
+    return 0;
+} 
